8d30eb1e886dbc91210bd896b961533661351bfc
Jesus Salas <jesuse.salas@alumnos.udg.mx>
2020-09-24 08:19:47 -0500

    prueba de los signos zodiacales

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hola Mundo")
+signo = ["capricornio", "acuario", "piscis", "aries", "tauro", "geminis",
+        "cancer","leo", "virgo", "libra", "escorpio", "sagitario"]
+fechas = [20, 19, 20, 20, 21, 20, 21, 23, 21, 21, 21, 21]
+
+dia = int(input())
+mes = int(input())
+
+mes = mes - 1
+if dia > fechas[mes]:
+    mes += 1
+if mes == 12:
+     mes = 0
+print(signo[mes])
